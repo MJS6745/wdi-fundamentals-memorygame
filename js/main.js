3275a27784417323237852068f2a7bbe44bcde36
Smets <n0180664@libp45p-32531dd.mis.lmig.com>
2017-08-28 15:32:02 -0400

    Created game logic

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//Start of code
+console.log("Up and running!");
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
